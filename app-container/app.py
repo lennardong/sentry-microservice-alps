1bb45893c5ff498e0b7cb8fdb2a263e4fc05bd05
lennardong <lennard.ong@gmail.com>
2023-05-05 15:06:24 +0800

    Implemented vision module
    - implementation using opencv and google vision api
    - missing test cases
    - missing documentation

--- app-container/app.py
+++ app-container/app.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Helloo, Worlds!"
 
-if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 8080)
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
